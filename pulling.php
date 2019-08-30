ae4ef7197bab359f09fbfe3cdf5a62934d0c8b83
Yidi Du <Yidi@Yidis-MacBook-Pro.local>
2019-08-29 22:02:33 -0400

    Complete pulling and display dummy data

--- pulling.php
+++ pulling.php
@@ -63,13 +63,7 @@ $sql = "CREATE TABLE timeperiod(
 	color Varchar(255)
 
 )";
-if ($conn->query($sql) === TRUE) {
-    echo "table create successfully<br>";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-
-/*$sql = "CREATE TABLE formation(
+$sql2 = "CREATE TABLE formation(
 	ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name Varchar(255),
 	period Varchar(255),
@@ -85,58 +79,112 @@ if ($conn->query($sql) === TRUE) {
 	depositional Text,
 	additional_info Text,
 	compiler Varchar(255)
-)";*/
-//$sql .= "USE myDB";
-//$sql .= "TRUNCATE TABLE timeperiod";
-//$sql = "TRUNCATE TABLE formation";
-//$sql = "TRUNCATE TABLE wells";
+)";
+
+if ($conn->query($sql)&&$conn->query($sql2) === TRUE) {
+    echo "table create successfully<br>";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+
 $sql = "INSERT INTO timeperiod(name,color)
 VALUES
-(	'Devonian',
+('Devonian',
 	'203/140/55'
+),
+('Quaternary',
+	'249/249/127'
+),
+(	'Neogene',
+	'255/230/25'
+)
+";
+
+$sql2 = "INSERT INTO formation(name,period,age_interval,province,type_locality,lithology,lower_contact,
+upper_contact,regional_extent,fossils,age,depositional,additional_info,compiler)
+VALUES
+(
+	'A’ertaxi Gr',
+	'Devonian',
+	'D22 (15); Givetian (late Middle Devonian)',
+	'Xinjiang',
+	'The type section is located at south of A’ertaxi Village, north side of Kunlun Mts. in the Xinjiang Uygur Autonomous Region. . It was named by No 13 Geological Team of Xinjiang in 1957 and was published by Editorial Board of Xinjiang Regional Stratigraphical Scale (1980).',
+	'Limestone, shale. The lower part of the Group is dominated by light-gray, dark-gray limestone and clayey shale, containing coral fossils. The upper part is characterized by green and black shale and dark-gray limestone with breccia limestone on its top. The thickness is 870 m. In the high mountain area between Longle-Agar River valley and Genlishalihe River, the group is characterized by gray, light greenish-gray quartzose sandstone, 260 to 900 m thick',
+	'Unknown: The contact relationships to the underlying strata are not yet clear.',
+	'Unknown: The contact relationships to the overlying strata are not yet clear.',
+	' ',
+	'Coral fossils: Eudophyllum sp., Brariphyllum sp., Syringopora sp., Temnophyllum sp.',
+	'Givetian (late Middle Devonian)',
+	' ',
+	' ',
+	'Wang Shitao'
+),
+(
+	'A’ertaxi Gr',
+	'Devonian',
+	'D22 (15); Givetian (late Middle Devonian)',
+	'Xinjiang',
+	'The type section is located at south of A’ertaxi Village, north side of Kunlun Mts. in the Xinjiang Uygur Autonomous Region. . It was named by No 13 Geological Team of Xinjiang in 1957 and was published by Editorial Board of Xinjiang Regional Stratigraphical Scale (1980).',
+	'Limestone, shale. The lower part of the Group is dominated by light-gray, dark-gray limestone and clayey shale, containing coral fossils. The upper part is characterized by green and black shale and dark-gray limestone with breccia limestone on its top. The thickness is 870 m. In the high mountain area between Longle-Agar River valley and Genlishalihe River, the group is characterized by gray, light greenish-gray quartzose sandstone, 260 to 900 m thick',
+	'Unknown: The contact relationships to the underlying strata are not yet clear.',
+	'Unknown: The contact relationships to the overlying strata are not yet clear.',
+	' ',
+	'Coral fossils: Eudophyllum sp., Brariphyllum sp., Syringopora sp., Temnophyllum sp.',
+	'Givetian (late Middle Devonian)',
+	' ',
+	' ',
+	'Wang Shitao2'
 )";
-if ($conn->query($sql) === TRUE) {
+
+if ($conn->query($sql)&&$conn->query($sql2) === TRUE) {
     echo "Insert successfully<br>";
 } else {
     echo "Error insert: " . $conn->error;
 }
 
-/*$sql .= "INSERT INTO timeperiod(name,color)
-VALUES
-(	'Quaternary',
-	'249/249/127'
-)";
 
-$sql = "INSERT INTO timeperiod(name,color)
-VALUES
-(	'Neogene',
-	'255/230/25'
-)";*/
-//$sql = "USE myDB";
-// Create connection
-// Create connection
-
-
-
-$sql = "SELECT * FROM timeperiod";
+/*$sql = "SELECT * FROM timeperiod";
 $result = mysqli_query($conn,$sql);
  while ($row = mysqli_fetch_array($result))
             {
                 $name  = $row['name'];
                 echo "name = $name<br>";
                 $color = $row['color'];
-                echo "color = $color";
+                echo "color = $color<br>";
+            }*/
+$sql = "SELECT * FROM formation";
+$result = mysqli_query($conn,$sql);
+ while ($row = mysqli_fetch_array($result))
+            {
+                $name  = $row['name'];
+                echo "name = $name<br>";
+                $period = $row['period'];
+                echo "period = $period<br>";
+                $age_interval = $row['age_interval'];
+                echo "age_interval = $age_interval<br>";
+                $province = $row['province'];
+                echo "province = $province<br>";
+                $type_locality = $row['type_locality'];
+                echo "type_locality = $type_locality<br>";
+                $lithology = $row['lithology'];
+                echo "lithology = $lithology<br>";
+                $lower_contact = $row['lower_contact'];
+                echo "lower_contact = $lower_contact<br>";
+                $upper_contact = $row['upper_contact'];
+                echo "upper_contact = $upper_contact<br>";
+                $regional_extent = $row['regional_extent'];
+                echo "regional_extent = $regional_extent<br>";
+                $fossils = $row['fossils'];
+                echo "fossils = $fossils<br>";
+                $depositional = $row['depositional'];
+                echo "depositional = $depositional<br>";
+                $additional_info = $row['additional_info'];
+                echo "additional_info = $additional_info<br>";
+                $compiler = $row['compiler'];
+                echo "compiler = $compiler<br><br><br>";
             }
-//if ($result->num_rows > 0) {
-    // output data of each row
-    //while($row = $result) {
-        //echo "$row";
-        echo $result["name"];
-        echo $result["color"];
-    //}
-//} else {
-    //echo "0 results";
-//}
+
 $conn->close();
 ?>
 </body>
