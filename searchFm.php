809e4f85a734826ad453fbb8362e6f747d015981
pvelpur <pvelpur@purdue.edu>
2019-09-19 11:44:19 -0400

    Create a page to search for formations

---
+++ searchFm.php
@@ -0,0 +1,25 @@
+<html>
+<title>Search for Formation</title>
+
+<style>
+    #searchbar {
+        border: 3px solid #CC99FF;
+        height: 40px;
+        width: 300px;
+    }
+    #submitbtn {
+        height: 40px;
+        border: 3px solid #000000;
+    }
+    
+</style>
+
+<body>
+<h3><b>Enter Formation Name Below: </b></h3>
+<form action="searchFm.php" method="post">
+    <input id="searchbar" type="text" name="search" placeholder="Search Formation Name...">
+    <input id="submitbtn" type="submit" value="Submit">
+</form>
+
+</body>
+</html>
