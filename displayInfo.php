08bf8cf50e72dad4248c8c90c21536ed988f9243
Yuzheng Qian <qian108@purdue.edu>
2019-10-09 22:50:55 +0000

    partially un-hardcode displayInfo.php

--- displayInfo.php
+++ displayInfo.php
@@ -1,39 +1,64 @@
 <!DOCTYPE html>
 <html>
+<?php 	include("SearchBar.php");
+	include("SqlConnection.php");
+?>
 <head>
 	<title>Amunik Fm</title>
 </head>
 <body>
+<?php echo "_GET[formation]";
+print_r($_REQUEST);
+$formationName = $_REQUEST;
+$sql = "SELECT * FROM formation WHERE name LIKE '%$formationName%'";
+$result = mysqli_query($conn, $sql);
+while($row = mysqli_fetch_array($result)) {
+	$name = $row['name'];
+        $period = $row['period'];
+        $age_interval = $row['age_interval'];
+        $province = $row['province'];
+        $type_locality = $row['type_locality'];
+        $lithology = $row['lithology'];
+	$lower_contact = $row['lower_contact'];
+	// add more
+}
+?>
 
-	<h1><b>Amunik Fm</b></h1>
-	<hr>
+
+
+
+
+
+
+
+	<h1><b><?=$name?></b></h1>
+    <hr>
     <br>
 
 	<h3><b>Period</b></h3>
     <hr>
-    <p>Devonian</p>
+	<p><?=$period?></p>
     <br>
-    <h3><b>Age Interval (Map column)</b></h3>
+	<h3><b>Age Interval (Map column)</b></h3>
     <hr>
-    <p>D<sub>3</sub> (18); Late Devonian</p>
+	<p><?=$age_interval?></p>
     <br>
-    <h3><b>Province</b></h3>
+	<h3><b>Province</b></h3>
     <hr>
-    <p>Qinghai</p>
+	<p><?=$province?></p>
     <br>
     <h3><b>Type Locality and Naming</b></h3>
     <hr>
-    <p>The type section is located at Amunik Mt., southeast of Xitieshan and southwest of Delingha, in the northern margin of Qaidam Basin, Qinghai Province. It was named by Qinghai No 1 Team of Regional Geological Survey in 	1976</p>
+    <p><?=$type_locality?> </p>
     <br>
     <h3><b>Lithology and Thickness</b></h3>
     <hr>
-    <p>Sandstone, conglomerate. The formation is mainly characterized by purple, grayish-purple, gray, yellowish-pink arkosic quartzose sandstone and silty mudstone. At the bottom is a bed of purple conglomerate about 2 m thick. The composition of the conglomerate is mainly chert, volcanic rock and limestone, with the pebbles being 2-4 cm in diameter, angular to subangular in shape and cemented with sands.<br>
-In Amunik section, the formation is 116 m in thickness.</p>
+    <p><?=$lithology?></p>
     <br>
     <h3><b>Relationships and Distribution</b></h3>
     <hr>
     <h4><i>Lower contact</i></h4>
-    <p>Disconformable contact with the underlying Maoniushan Fm.</p>
+    <p><?=$lower_contact?></p>
     <h4><i>Upper contact</i></h4>
     <p>Conformable or disconformable contact to the overlying Chengqianggou Fm of Lower Carboniferous.</p>
     <h4><i>Regional extent</i></h4>
