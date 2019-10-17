b8ff749cdd56dd30074f763731367c89af5cda88
pvelpur <pvelpur@purdue.edu>
2019-10-16 22:22:31 -0400

    center the searchBar

--- SearchBar.php
+++ SearchBar.php
@@ -5,20 +5,31 @@
     #searchbar {
         border: 3px solid #CC99FF;
         height: 40px;
-        width: 300px;
+        width: 500px;
+        
     }
     #submitbtn {
         height: 40px;
         border: 3px solid #000000;
     }
+
+    .search-container{
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-top: 10px;
+    }
     
 </style>
 
 <body>
-<h3><b>Enter Formation Name Below: </b></h3>
-<form action="searchFm.php" method="post">
-    <input id="searchbar" type="text" name="search" placeholder="Search Formation Name...">
-    <input id="submitbtn" type="submit" value="Submit">
-</form>
+    <div class = "search-container">
+        
+        <form action="searchFm.php" method="post">
+        <input id="searchbar" type="text" name="search" placeholder="Search Formation Name...">
+        <input id="submitbtn" type="submit" value="Submit">
+        </form>
+    </div>
 </body>
 </html>
