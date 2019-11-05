4b729f6e64ab223c8818cade930d05c33c4b3457
Yuzheng Qian <qian108@purdue.edu>
2019-11-05 17:38:44 +0000

    make display more compact by removing most of the <hr>

--- site/displayInfo.php
+++ site/displayInfo.php
@@ -19,18 +19,6 @@ while($row = mysqli_fetch_array($result)) {
 	$name = $row['name'];
         $period = $row['period'];
         $age_interval = trim($row['age_interval']);
-
-	/*
-	if(preg_match("/^([A-Za-z])([0-9]{1-2})$/", $age_interval)) {
-		$output = $age_interval[0];
-		$output .= "<sub>$age_interval[1]<\sub>";
-		if(preg_match("/^[0-9]$/", $age_interval[2])) { //<-------
-			$output .= "<sup>$age_interval[2]<\sub>";
-		}
-		$age_interval = $output;
-	}
-	*/
-
         $province = $row['province'];
         $type_locality = $row['type_locality'];
         $lithology = $row['lithology'];
@@ -55,50 +43,38 @@ if($name == "") {
 
 // display information below
 ?>
-    <div>
+    <div id="title">
 	<h1><b><?=$name?></b></h1>
         <hr>
     </div>
 
-<!--
-	<h4><?=$period?></h4>
-	<h4><?=$age_interval?></h4>
-	<h4><?=$province?> <br></h4>
--->
-	
     <div id="period">
-	<h3><b>Period</b></h3>
-      <hr>
+	<h3><b>Period: </b></h3>
 	<p><?=$period?><br></p>
     </div>
 
     <div id="age_interval">
-	<h3><b>Age Interval</b></h3>
-      <hr>
+	<h3><b>Age Interval: </b></h3>
 	<p><?=$age_interval?><br></p>
     </div>
 
     <div id="province">
-	<h3><b>Province</b></h3>
-    <hr>
+	<h3><b>Province: </b></h3>
 	<p><?=$province?><br></p>
     </div>
 
     <div id="type_locality">
 	<h3><b>Type Locality and Naming</b></h3>
-    <hr>
 	<p><?=$type_locality?><br></p>
     </div>
 
     <div id="lithology">
 	<h3><b>Lithology and Thickness</b></h3>
-    <hr>
 	<p><?=$lithology?><br></p>
     </div>
 
     <div id="relationships_distribution">
 	<h3><b>Relationships and Distribution</b></h3>
-    <hr>
 	<div id="lower_contact">
 		<h4><i>Lower contact</i></h4>
 		<p><?=$lower_contact?></p>
@@ -115,31 +91,26 @@ if($name == "") {
 
     <div id="fossils">
 	<h3><b>Fossils</b></h3>
-    <hr>
 	<p><?=$fossils?><br></p>
     </div>
 
     <div id="age">
 	<h3><b>Age</b></h3>
-    <hr>
 	<p><?=$age?><br></p>
     </div>
 
     <div id="depositional">
 	<h3><b>Depositional setting</b></h3>
-    <hr>
 	<p><?=$depositional?><br></p>
     </div>
 
     <div id="additional_info">
 	<h3><b>Additional Information</b></h3>
-    <hr>
 	<p><?=$additional_info?><br></p>
     </div>
 
     <div id="compiler">
 	<h3><b>Compiler</b></h3>
-    <hr>
 	<p><?=$compiler?><br></p>
     </div>
 
