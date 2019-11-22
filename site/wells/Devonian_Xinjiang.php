8b5f1b689a3e836191244e04e41dfda435238c4e
Yidi Du <Yidi@pal-nat186-74-196.itap.purdue.edu>
2019-11-22 11:43:46 -0500

    Modified the devonian_xinjiang.php and it can now link to another page

--- site/wells/Devonian_Xinjiang.php
+++ site/wells/Devonian_Xinjiang.php
@@ -2,7 +2,7 @@
   <head>
   	<title>Devonian_Xinjiang</title>
   	<body>	
-  	  <a href="displayInfo.php?formation=Aertemeishibulak%20Fm">
+  	  <a href="../displayInfo.php?formation=Aertemeishibulak%20Fm">
        <img src="../Mapinfo/cat_wells.jpg" width="800" height="625" border="0" id="Devonian_Xinjiang" />
       </a>
   	</body>
