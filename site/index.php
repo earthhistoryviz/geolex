7e77fd6ebb7be2e8398be746c1c745966139977a
pvelpur <pvelpur@purdue.edu>
2019-12-03 18:28:48 -0500

    make index page a little more vibrant

--- site/index.php
+++ site/index.php
@@ -4,6 +4,8 @@
 <title>DevonianTest</title>
 </head>
 	<?php include("navBar.php");?>
+	<h2 align="center" style="color:blue;">Welcome to the International Geology Website and Database! <br>Please enter a formation name or group to retrieve more information.</h2>
+	<?php include("SearchBar.php"); ?>
 <body>
 
 <form>
