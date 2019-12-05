bd236c5a87b6495d1c8c82e4c82dc6f0cfdda4dd
Yidi Du <Yidi@Yidis-MacBook-Pro.local>
2019-12-05 17:08:01 -0500

    adding Guangxi Dovonian for wells

--- site/wells/Devonian_Xinjiang.php
+++ site/wells/Devonian_Xinjiang.php
@@ -3,7 +3,7 @@
   	<title>Devonian_Xinjiang</title>
   	<body>	
   	  <a href="../displayInfo.php?formation=Aertemeishibulak%20Fm">
-       <img src="../Mapinfo/cat_wells.jpg" width="800" height="625" border="0" id="Devonian_Xinjiang" />
+       <img src="wells_pic/cat_wells.jpg" width="800" height="625" border="0" id="Devonian_Xinjiang" />
       </a>
   	</body>
   </head>
