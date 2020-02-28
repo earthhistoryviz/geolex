34fd3e79596a89b20c94f69c61e87ac8538e3323
Yuzheng Qian <qian108@purdue.edu>
2020-02-28 01:12:11 -0500

    Add support for Guizhou Province on the Devonian map

---
+++ site/wells/Devonian_Guizhou.php
@@ -0,0 +1,38 @@
+<?php
+$page = "allformations_new";
+// include("../full/config/config.php");
+// include("lib/formationtop_lib.php");
+include("../formationheader.php");
+?>
+
+<img src="wells_pic/Guizhou_Devonian.jpg" width="700" height="1000" border="0" usemap="#Map" />
+<map name="Map" id="Map">
+<area shape="rect" coords="250,65,355,77" href="../displayInfo.php?formation=Tangbagou%20Fm" />
+<area shape="rect" coords="592,443,697,526" href="../displayInfo.php?formation=Huohong%20Fm" />
+<area shape="rect" coords="137,442,242,525" href="../displayInfo.php?formation=Huohong%20Fm" />
+<area shape="rect" coords="366,142,470,225" href="../displayInfo.php?formation=Zhewang%20Fm" />
+<area shape="rect" coords="366,79,471,141" href="../displayInfo.php?formation=Gelaohe%20Fm" />
+<area shape="rect" coords="593,241,698,441" href="../displayInfo.php?formation=Liujiang%20Fm" />
+<area shape="rect" coords="593,81,699,242" href="../displayInfo.php?formation=Wuzhishan%20Fm" />
+<area shape="rect" coords="479,80,584,284" href="../displayInfo.php?formation=Wuzhishan%20Fm" />
+<area shape="rect" coords="138,242,243,442" href="../displayInfo.php?formation=Liujiang%20Fm" />
+<area shape="rect" coords="480,443,584,517" href="../displayInfo.php?formation=Huohong%20Fm" />
+<area shape="rect" coords="252,223,357,466" href="../displayInfo.php?formation=Gaopochang%20Fm" />
+<area shape="rect" coords="366,224,471,283" href="../displayInfo.php?formation=Yaosuo%20Fm" />
+<area shape="rect" coords="365,284,470,440" href="../displayInfo.php?formation=Wangchengpo%20Fm" />
+<area shape="rect" coords="366,442,470,493" href="../displayInfo.php?formation=Dushan%20Fm" />
+<area shape="rect" coords="137,528,242,657" href="../displayInfo.php?formation=Guanziyao%20Fm" />
+<area shape="rect" coords="366,494,472,544" href="../displayInfo.php?formation=Bangzhai%20Fm" />
+<area shape="rect" coords="595,527,698,682" href="../displayInfo.php?formation=Chehe%20Fm" />
+<area shape="rect" coords="366,544,472,604" href="../displayInfo.php?formation=Longdongshui%20Fm" />
+<area shape="rect" coords="366,646,471,745" href="../displayInfo.php?formation=Danlin%20Fm" />
+<area shape="rect" coords="365,604,471,647" href="../displayInfo.php?formation=Shujiaping%20Fm" />
+<area shape="rect" coords="480,284,584,441" href="../displayInfo.php?formation=Xiangshuidong%20Fm" />
+<area shape="rect" coords="253,468,339,518" href="../displayInfo.php?formation=Mazongling%20Fm" />
+<area shape="rect" coords="253,520,340,574" href="../displayInfo.php?formation=Wudang%20Fm" />
+<area shape="rect" coords="340,467,356,571" href="../displayInfo.php?formation=Mangshan%20Gr" />
+<area shape="rect" coords="253,141,356,223" href="../displayInfo.php?formation=Zhewang%20Fm" />
+<area shape="rect" coords="251,78,356,140" href="../displayInfo.php?formation=Gelaohe%20Fm" />
+<area shape="rect" coords="138,80,244,241" href="../displayInfo.php?formation=Wuzhishan%20Fm" />
+<area shape="rect" coords="365,65,470,77" href="../displayInfo.php?formation=Tangbagou%20Fm" />
+</map>
