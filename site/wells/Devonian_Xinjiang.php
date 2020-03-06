bc5d52244ca1a7fd891010bd812f8a027202109b
Yuzheng Qian <qian108@purdue.edu>
2020-03-06 11:50:52 -0500

    removed cat image for Devonian Xinjiang

--- site/wells/Devonian_Xinjiang.php
+++
@@ -1,9 +0,0 @@
-  <html>
-  <head>
-  	<title>Devonian_Xinjiang</title>
-  	<body>	
-  	  <a href="../displayInfo.php?formation=Aertemeishibulak%20Fm">
-       <img src="wells_pic/cat_wells.jpg" width="800" height="625" border="0" id="Devonian_Xinjiang" />
-      </a>
-  	</body>
-  </head>
