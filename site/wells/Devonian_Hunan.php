1451c1c9b5c3438e0cb0c9298571a4e73bbdf28d
Root on dev server <root@dev.timescalecreator.com>
2020-07-10 16:10:00 +0000

    Fixed Xikuangshan province

--- site/wells/Devonian_Hunan.php
+++ site/wells/Devonian_Hunan.php
@@ -11,13 +11,13 @@ include("./wells-header.php");
 <area shape="rect" coords="220,417,307,455" href="../displayInfo.php?formation=Qiziqiao%20Fm" />
   <area shape="rect" coords="219,455,307,485" href="../displayInfo.php?formation=Huanggongtang%20Fm" />
   <area shape="rect" coords="220,270,308,416" href="../displayInfo.php?formation=Shetianqiao%20Fm" />
-  <area shape="rect" coords="218,138,310,269" href="../displayInfo.php?formation=Xikuangshang%20Fm" />
+  <area shape="rect" coords="218,138,310,269" href="../displayInfo.php?formation=Xikuangshan%20Fm" />
 <area shape="rect" coords="218,487,308,564" href="../displayInfo.php?formation=Tiaomajian%20Fm" />
 <area shape="rect" coords="219,81,310,138" href="../displayInfo.php?formation=Menggongao%20Fm" />
   <area shape="rect" coords="123,81,212,100" href="../displayInfo.php?formation=Menggongao%20Fm" />
   <area shape="rect" coords="120,100,213,119" href="../displayInfo.php?formation=Shaodong%20Fm" />
   <area shape="rect" coords="123,120,212,139" href="../displayInfo.php?formation=Oujiachong%20Fm" />
-  <area shape="rect" coords="122,140,212,222" href="../displayInfo.php?formation=Xikuangshang%20Fm" />
+  <area shape="rect" coords="122,140,212,222" href="../displayInfo.php?formation=Xikuangshan%20Fm" />
   <area shape="rect" coords="122,222,210,268" href="../displayInfo.php?formation=Changlongjie%20Fm" />
   <area shape="rect" coords="123,306,213,364" href="../displayInfo.php?formation=Longkouchong%20Fm" />
   <area shape="rect" coords="123,268,212,305" href="../displayInfo.php?formation=Qilijiang%20Fm" />
