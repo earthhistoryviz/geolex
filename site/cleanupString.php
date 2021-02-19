a76517e65ba8641b409d83282a630f0fee8749c9
Suyash <mishr100@dev.timescalecreator.com>
2021-02-19 18:29:34 +0000

    Friday meeting edits

--- site/cleanupString.php
+++ site/cleanupString.php
@@ -4,4 +4,22 @@ function cleanupString($str) {
   return strtoupper(trim(preg_replace("/<[^>]+>/", "", $str)));
 }
 
+function cleanupGeojson($str){
+	/*
+	$str = preg_replace("/(&quot|&QUOT/)", '"', $str);
+	$str = preg_replace("/(/</p/>|/<p/>)/", "", $str);
+	$str = preg_replace("/(/<strong/>|/</strong/>)/", "", $str);
+	 */
+        /*
+	$str = strip_tags($str, '<strong></strong><p></p>');
+	$str = strip_tags($str, '&quot');
+	$str = strip_tags($str, '&QUOT');
+	 */
+	$str = preg_replace("/(&QUOT;|&quot;)/", '"', $str);
+	$str = strip_tags($str);
+	//$str = preg_replace("/NULL/", '"NULL"', $str);
+	$str = preg_replace('/"COORDINATES"/',' "coordinates"', $str);
+	return $str;
+}
+
 ?>
