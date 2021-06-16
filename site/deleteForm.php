4d6b737ded06886017082e4bbf0bb4ba6d3a2087
Suyash <mishr100@dev.timescalecreator.com>
2021-06-16 19:16:22 +0000

    allowing deletion of formation with special characters needing to be escaped

--- site/deleteForm.php
+++ site/deleteForm.php
@@ -3,7 +3,8 @@ include_once("SqlConnection.php");
 include_once("navBar.php");
 include_once("SearchBar.php");
 $title = $_POST["name"];echo $title;
-$sql = "DELETE FROM formation WHERE name = '$title';";
+//$sql = "DELETE FROM formation WHERE name = '$title';";
+$sql = sprintf("DELETE FROM formation WHERE name='%s'", mysqli_real_escape_string($conn, $title));
 echo "<pre> $sql <\pre>";
 
 if($conn->query($sql)=== TRUE){
