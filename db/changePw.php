37a5d973f726c27b2ba968a4a92afc5180ea7e52
Sabrina <sabrina6933@gmail.com>
2023-03-12 08:16:32 +0000

    change password

---
+++ db/changePw.php
@@ -0,0 +1,21 @@
+<?php
+// This file gives you the $conn variable:
+include("../site/SqlConnection.php");
+
+if ($argc != 2) {
+  echo "USAGE: $argv[0] <newpassword>";
+  exit;
+}
+
+$newpw = password_hash($argv[1], PASSWORD_DEFAULT);
+$salt = "SALT";
+$sql = "UPDATE user_info
+            SET pasw='$newpw'
+          WHERE uname='root' OR uname='root2'";
+if ($conn->query($sql) === TRUE) {
+  echo "Password updated successfully\n";
+} else {
+  echo "ERROR: updating password: " . $conn->error . "\n";
+}
+
+
