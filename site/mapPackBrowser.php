ad72ce222df64f0033ec4bb13fd117abaec8d7f5
Sabrina <sabrina6933@gmail.com>
2023-04-11 11:43:57 +0000

    adding tsc parser

---
+++ site/mapPackBrowser.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php 
+error_reporting(E_ALL);
+include_once("navBar.php");
+include_once("SearchBar.php");
+include_once("SqlConnection.php");
+include_once("TimescaleLib.php");
+
+
+
+if (!$_SESSION["loggedIn"]) {
+    echo "ERROR: You must be logged in to access this page.";
+    exit(0);
+}
+
+?>
+<br><br> 
+
+<form action="" method="post" enctype="multipart/form-data">
+    Select the Map Pack to upload:
+    <span style="color:red;" class="req">*</span>
+    <input type="file" name="mapPackDoc" id="mapPackDoc" required >
+    <br><br>
+
+    Select Data Pack to upload:
+    <span style="color:red;" class="req">*</span>
+    <input type="file" name="dataPackDoc" id="dataPackDoc" required >
+    <br><br>
+    <input type="submit" value="Upload MapPack and DataPack File" name="submit">
+</form>
+
+
+<?php
+    //function to create a diamond shaped GeoJSON given the lat and long of a formation
+    function createGeoJSONFromLatLon(float $lon, float $lat) {
+        $geoJsonString = "{\"type\":\"Feature\",\"geometry\":{\"type\":\"MultiPolygon\",\"coordinates\":[[[";
+        
+        //The strings in php are annoying, they are hard to do arithmetic inside of quotes..
+
+        //lat+1
+        $lat_add1 = $lat + 1;
+        //lat-1
+        $lat_sub1 = $lat -1;
+        //lon+1
+        $lon_add1 = $lon + 1;
+        //lon-1
+        $lon_sub1 = $lon - 1;
+
+        //adds to string a geoJSON that is converted
+        //it makes a diamond shape, the original point is (lat,lon)
+        //
+        $geoJsonString .= "[$lon,$lat_add1],[$lon_sub1,$lat],[$lon,$lat_sub1],[$lon_add1,$lat],[$lon,$lat_add1]";
+
+        $geoJsonString .= "]]]}}";
+
+        return $geoJsonString;
+
+    }
+
+     //function to find if duplicate formation's exist 
+    function duplicateFormationExist(array $objects, $property, $value): bool {
+        foreach ($objects as $object) {
+            if (property_exists($object, $property) && $object->{$property} === $value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function calculatePercentUp($timescale, $date) {
+        $previous_scale = 0;
+        
+        foreach( $timescale as $scale) {
+            $current_scale = $scale['base'];
+            
+            //echo $scale['base'] . "\n";
+
+            if($date <= $current_scale) {
+                //calculating the percent up
+                $percentUp = ($current_scale - $date) / ($current_scale - $previous_scale);
+                return round($percentUp, 2);
+                
+            }
+
+            $previous_scale = $current_scale;
+            
+        }    
+
+    }
+
+    function calculateStage($timescale, $date) {
+        $previous_scale = 0;
+        
+        foreach( $timescale as $scale) {
+            $current_scale = $scale['base'];
+            
+            if($date <= $current_scale) {
+                //calculating the stage
+                return $scale['stage'];                
+            }
+            
+        }    
+
+    }
+
+    function calculatePeriod($timescale, $date) {
+        $previous_scale = 0;
+        
+        foreach( $timescale as $scale) {
+            $current_scale = $scale['base'];
+            
+            if($date <= $current_scale) {
+                //calculating the stage
+                return $scale['period'];                
+            }
+            
+        }    
+
+    }
+
+    function updateStage($timescale, $form) {
+
+        $form->frac_upB = calculatePercentUp($timescale, $form->begin_date);
+        $form->frac_upE = calculatePercentUp($timescale, $form->end_date);
+
+        $form->beginning_stage = calculateStage($timescale, $form->begin_date);
+        $form->end_stage = calculateStage($timescale, $form->end_date);   
+        
+        $form->period = calculatePeriod($timescale, $form->begin_date);   
+    }
+
+    if(isset($_POST['submit'])){        
+        try {
+            if (!file_exists('./uploads1')) {
+                mkdir('./uploads1', 0777, true); //if the uploads1 folder does not exist, create it, set the permissions of the directory
+            }
+            $uploads_dir ='uploads1';
+        
+        
+            // Undefined | Multiple Files | $_FILES Corruption Attack
+            // If this request falls under any of them, treat it invalid.
+            if (!isset($_FILES['mapPackDoc']['error']) || is_array($_FILES['mapPackDoc']['error'])) {
+                throw new RuntimeException('Invalid parameters.');
+            }
+            if (!isset($_FILES['dataPackDoc']['error']) || is_array($_FILES['dataPackDoc']['error'])) {
+                throw new RuntimeException('Invalid parameters.');
+            }
+        
+                // You should also check filesize here.
+            if ($_FILES['mapPackDoc']['size'] > 2097152 || $_FILES['dataPackDoc']['size'] > 2097152) {
+                throw new RuntimeException('Exceeded filesize limit.');
+            }
+        
+            //gets the 2 required files name and replace all spaces with underscores
+            $mapPackName = str_replace(' ', '_', $_FILES["mapPackDoc"]["name"]);
+            $dataPackName = str_replace(' ', '_', $_FILES["dataPackDoc"]["name"]);
+        
+            $mapPackFolderName = $_FILES["mapPackDoc"]["tmp_name"];
+            $dataPackFolderName = $_FILES["dataPackDoc"]["tmp_name"];
+        
+            $mapPackNewPath = "./$uploads_dir/$mapPackName";
+            $dataPackNewPath = "./$uploads_dir/$dataPackName";
+        
+        
+            //if(move_uploaded_file($mapPackFolderName,"./$uploads_dir/$mapPackName"))
+            if(!move_uploaded_file($mapPackFolderName, $mapPackNewPath)) {
+                echo 'Failed to move MAPPACK File from temp file of PHP. FAILURE IN FILE Movement <br>';
+                print_r(error_get_last());
+            }
+        
+            //if(move_uploaded_file($dataPackFolderName,"./$uploads_dir/$dataPackName"))
+            if(!move_uploaded_file($dataPackFolderName, $dataPackNewPath)) {
+                echo 'Failed to move DATAPACK File from temp file of PHP. FAILURE IN FILE Movement <br>';
+                print_r(error_get_last());
+            }
+        
+        
+            $cmd = "./mapPackParser.py ". "$mapPackNewPath" . " $dataPackNewPath" ." $mapPackName" ." 2>&1";
+            $hello = exec($cmd, $output, $ending);
+            print_r($output[0]);
+        
+        
+            if($ending > 0) {
+                echo "Python returned ($ending): <pre>";
+                print_r($output[0]);
+                echo " And here is the command that generated it: $cmd</pre>";
+              }
+
+            //echo("Successfully Parsed the MapPack and DataPack... <br><br>"); 
+            
+            //The parsed JSON file is called formations.json in uploads1 directory
+            $formationJSON = './uploads1/' . str_replace(" ", "", substr($mapPackName,0, 8)) .'_formations.json';
+            
+            $json_raw = file_get_contents($formationJSON);
+            $full_formation_list = json_decode($json_raw, true); //contains all of the formations parsed from the datapack, including duplicate names         
+            
+
+            #create an array of the duplicate formations removed and the end_date "correct" by comparing all of the end age of the duplicate formations and choosing the newest date (oldest date = begin,  newest date = end because older rock is on the bottom... )
+            $formation_set = [];
+
+            foreach($full_formation_list as $formation_list) {
+                // Compiling all geoJSON strings from the returned formations into recon.geojson
+                
+                if (!duplicateFormationExist($formation_set, 'name', $formation_list['name'])) {  //if it is NOT a duplicate formation
+                    //add the formation to the formation_set array
+
+                    // echo"<pre> Formation_List =";
+                    // print_r($formation_list);
+                    // echo"</pre>";
+
+                    array_push($formation_set, (object)[
+                        'name' => $formation_list['name'],
+                        'begin_date' => $formation_list['begin_date'],
+                        'end_date' => $formation_list['end_date'],
+                        'lithology_pattern' => $formation_list['lithology_pattern'],
+                        'formation_description' => $formation_list['formation_description'],
+                        'province' => $formation_list['province'],
+                        'long_lat' => "long: " .$formation_list['longitude'] ." lat: ".$formation_list['latitude'],
+                        'GeoJSON' => createGeoJSONFromLatLon($formation_list['longitude'], $formation_list['latitude']),
+                        'frac_upB' => "",
+                        'frac_upE' => "",
+                        'beginning_stage' => "",
+                        'end_stage' => "",
+                        'period' => "",
+                        'type_locality' => "Column: " .$formation_list['column'],
+                        ]);
+
+
+                } else {
+
+                    //loop through formation_set and get the formation object
+                    //replace ONLY if new_begindate is greater than current begin_date 
+                    //replace ONLY if new_ENDdate is less than current end_date
+
+                    foreach($formation_set as $replace_formation) {
+                        if($formation_list['name'] == $replace_formation->name) {
+                            $beginDateInSet = $replace_formation->begin_date;
+                            $endDateInSet = $replace_formation->end_date;
+
+                            $beginDateInList = $formation_list['begin_date']; //new_begindate
+                            $endDateInList = $formation_list['end_date']; //new_enddate
+
+                            if($beginDateInSet < $beginDateInList) {
+                                $replace_formation->begin_date = $formation_list['begin_date'];
+                            }
+
+                             if($endDateInSet > $endDateInList) {
+                                $replace_formation->end_date = $formation_list['end_date'];
+                             }
+                        }
+
+                    }
+
+                    
+                }           
+            }
+
+            // echo"<pre> UPDATED_FORMATION =";
+            // print_r($formation_set);
+            // echo"</pre>";
+
+
+
+
+            // Load the default timescale
+            $timescale = parseDefaultTimescale();
+            if (!$timescale) {
+                throw new RuntimeException("Failed to parse the default timescale.");
+            } else {
+                echo "<br> <br> Successfully parsed default timescale <br>";
+                
+                
+                echo "end of timescale";
+            }
+
+            foreach($formation_set as $form) {
+                              
+                updateStage($timescale, $form);
+             }
+           
+            
+           
+            timeScaleMapPackUpload($formation_set); //insert into SQL
+
+        
+        } catch (RuntimeException $e) {
+            echo $e->getMessage();
+        }
+        
+    }
+
+?>
+
+</body>
+</html>
---
+++ site/mapPackParser.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import csv
+import json
+import sys
+import re
+
+mapPackPath = sys.argv[1]
+dataPackPath = sys.argv[2]
+
+class Formation():
+    def __init__(self, name, begin_date, end_date, lithology_pattern, formation_description, column, longitude, latitude, province):
+        self.column = fixColumnName(column)
+        self.name = fixName(name, self.column)
+        self.begin_date = begin_date
+        self.end_date = end_date
+        self.lithology_pattern = lithology_pattern
+        self.formation_description = formation_description
+        self.province = province
+        self.latitude = latitude
+        self.longitude = longitude
+
+#Encodes the formation class to json
+class FormationEncoder(json.JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+def get_province(province_dict, val):
+
+    for province, list_of_columns in province_dict.items():
+        if val in list_of_columns: #value is a list of column names
+            return province
+
+def fixColumnName(column):
+    column = column.replace("-", " ")
+    column = column.replace("_", " ")
+    regex = re.compile("[^a-zA-Z?& ']")
+    #First parameter is the replacement, second parameter is your input string
+    return (regex.sub('', column)).title()
+
+
+
+def fixName(name, column):
+    regex = re.compile("[^a-zA-Z?& ']")
+    #First parameter is the replacement, second parameter is your input string
+    name = (regex.sub('', name)).title()
+    namelist = name.split()
+    
+    namelist.insert( len(namelist)-1, column)
+    return ' '.join(namelist)
+
+#reads the datapack and returns a json 
+def reading_datapack(columns_dict):
+    try:
+        list_of_formation = []
+        last_age = -1
+        current_column = ""
+        state = "NOT_IN_COLUMN"
+        lon_lat = (0,0)
+        province_dict = {}
+        current_prov = ""
+
+        str2 = dataPackPath[len(dataPackPath) - 3:] #see if the ending of the file
+
+        if str2 == '.csv': #if file is a .csv file
+            try:
+                with open(dataPackPath, newline='' ) as csvfile:
+                    data_file= list(csv.reader(csvfile, delimiter = ',')) #converts the read data file into a list for easy parsing.
+            except UnicodeDecodeError: #encoding is in UTF-16 instead
+                with open(dataPackPath, newline='', encoding='utf-16') as csvfile:
+                    data_file= list(csv.reader(csvfile, delimiter = ',')) #converts the read data file into a list for easy parsing.
+            
+        else: #not .csv file
+            try:
+                with open(dataPackPath, newline='' ) as csvfile: #default encoding
+                    data_file= list(csv.reader(csvfile, delimiter = '\t')) #converts the read data file into a list for easy parsing.
+
+            except UnicodeDecodeError:#encoding is in UTF-16 instead
+                with open(dataPackPath, newline='' , encoding='utf-16') as csvfile:      
+                    data_file= list(csv.reader(csvfile, delimiter = '\t')) #converts the read data file into a list for easy parsing.
+                    
+
+        #parsing the file and creating a list of objects
+        for line in data_file:
+            try:
+                if(len(line) < 10):
+                    line += [''] * (10 - len(line))
+
+                if(state == 'NOT_IN_COLUMN'):
+                    if(line[1] == 'facies'):
+                        state = 'READING_FACIES'
+                        current_column = line[0]
+                        current_prov = get_province(province_dict, current_column)
+                        lon_lat = columns_dict[current_column]
+                        continue
+                    
+                    else: 
+                        province_values = []
+                        if line[0] != '':
+                            for index in line:
+                                if index != '' and index != ':' and index != line[0]:
+                                    province_values.append(index)
+
+                            province_dict[line[0]] = province_values
+                    
+                if(state == 'READING_FACIES'):
+                    if all('' == s or s.isspace() for s in line):
+                        state = 'NOT_IN_COLUMN'
+                        continue
+                    if(line[2] != '' and line[2] != ' '):
+                        try:
+                            formation = Formation(line[2], float(line[3]), last_age, line[1],line[4], current_column, lon_lat[0], lon_lat[1], current_prov)
+                            list_of_formation.append(formation)
+                        except:
+                            print("FILE IS NOT VALID!")
+
+                    if(line[3] != ''):
+                        last_age = float(line[3])
+            except KeyError:
+                print('No ' + line[0] + 'in Mappack') #column might not exist in the MapPack so no coordinates
+                continue
+
+
+    except Exception as e:
+        print("INVALID data PACK!!!!")
+        return 1
+        
+    #Debugging info
+    # for formation in list_of_formation:
+    #     print("Lithology: ", formation.lithology_pattern, "Name: ", formation.name, "begin Date:", formation.begin_date, "End Date: ", formation.end_date, "lat_lon", formation.lat_lon)
+
+    return list_of_formation
+
+
+def reading_MapLocations():
+        index = 0
+        columns_dict = {} #dictionary that has the column's name as keys the values is a tuple of the column's lat and lon for the formation
+        start = False
+
+        #For example:
+        #index 0: lat and long of the column as values, the first value will be a latitude the second value will be a longitude 
+        #Belgium_TSCLiteMapLocations_12Dec2012.txt
+        try:
+            with open(mapPackPath, newline='') as csvfile:
+                data = list(csv.reader(csvfile, delimiter = '\t'))
+                for row in data:
+                    index += 1
+                    if(len(row) == 0):
+                        continue
+                    if(start): #this is because there are other header information in the file, this index is where the column names come in
+                        #column_dict key are the column names
+                        #column_dict values is a list of the column information 
+                        #row[2] = LAT row[3] = LON
+                        try:
+                            columns_dict[row[1]] = (float(row[3]), float(row[2])) #adding lon and lat as a tuple 
+                        except ValueError:
+                            continue
+                    if(row[1] == 'NAME'):
+                        start = True
+
+        except UnicodeDecodeError: #encoding is in UTF-16 instead
+            with open(mapPackPath, newline='', encoding='utf-16') as csvfile:
+                data = list(csv.reader(csvfile, delimiter = '\t'))
+                for row in data:
+                    index += 1
+                    if(len(row) == 0):
+                        continue
+                    if(start): #this is because there are other header information in the file, this index is where the col names come in
+
+                        #col_dict key are the col names
+                        #col_dict values is a list of the col information 
+                        #row[2] = LAT row[3] = LON
+                        
+                        try:
+                            cols_dict[row[1]] = (float(row[3]), float(row[2])) #adding lon and lat as a tuple 
+                        except ValueError:
+                            continue
+
+                    if(row[1] == 'NAME'):
+                        start = True
+    
+        except Exception as e:
+            print("INVALID MAP PACK!!!!")
+            return 1
+        
+        return columns_dict
+
+def main():
+    columns_dict = reading_MapLocations() #reads file number 1 and returns a dictionary that has the column's name as keys the values is a tuple of the column's lat and lon for the formation
+    list_of_formations = reading_datapack(columns_dict) #reads file number 2 and returns a list of object Formation
+
+    
+    index = 0
+    formationFileName = "./uploads1/" +(str(sys.argv[3])[0:8]).replace(" ", "") +"_formations.json"
+
+
+    with open(formationFileName, "w") as file1:
+        # Writing data to a file
+        file1.write("[\n")
+
+        for formation in list_of_formations:
+            
+            string = json.dumps(formation, indent=4, cls=FormationEncoder)
+            file1.write(string)
+            index += 1
+            if(index != len(list_of_formations)):
+                file1.write(",")
+            
+
+        file1.write("]")
+        
+        
+
+
+main()
