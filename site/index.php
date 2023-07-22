f79c38bf6e7bbe32ded9ff873bf59d69535012da
Yuzheng Qian <xdhqyz@gmail.com>
2023-07-22 04:46:37 +0000

    Update Override logic

--- site/index.php
+++ site/index.php
@@ -139,8 +139,8 @@ function sortByAge($a, $b) {
 
 <?php
 include_once("allowCustomOverride.php");
-if (allowCustomOverride(__FILE__)) {
-  return; // "true" means we had an override, do not continue execution of this script (i.e. top-level return)
+if (!allowCustomOverride(__FILE__)) { // "true" means we had an override. Override file will set the title message.
+  $titleMessage = "Welcome to the International Geology Website and Database!";
 }
 
 // Default welcome page:
@@ -151,7 +151,7 @@ if (allowCustomOverride(__FILE__)) {
 include("navBar.php"); ?>
 
 <h2 style="text-align: center; color: blue;">
-  Welcome to the International Geology Website and Database!<br>
+  <?=$titleMessage ?><br>
   Please enter a formation name or group to retrieve more information.
 </h2> <?php
 
