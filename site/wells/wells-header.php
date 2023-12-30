b3d668585f1972670ae4e7dccc5aeddff31335a0
asivath <asivatha@purdue.edu>
2023-12-30 20:11:12 +0000

    fixed header for wells

--- site/wells/wells-header.php
+++ site/wells/wells-header.php
@@ -1,5 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php
-include("../navBar.php");
+    $title = substr($_SERVER['REQUEST_URI'], 7);
+    $title = str_replace(['.php', '_'], ['', ' '], $title); ?>
+    <title><?= $title ?></title> <?php
+    include("../navBar.php");
 ?>
 
 <div style="margin-left: 10px; padding-left: 10px;">
