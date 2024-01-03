2f00f456d9543bbb77343e86e1393bf262ed8458
asivath <asivatha@purdue.edu>
2024-01-03 18:32:45 +0000

    fixes to styling

--- site/wells/wells-header.php
+++ site/wells/wells-header.php
@@ -6,10 +6,17 @@
 <?php
     $title = substr($_SERVER['REQUEST_URI'], 7);
     $title = str_replace(['.php', '_'], ['', ' '], $title); ?>
-    <title><?= $title ?></title> <?php
+    <title><?= $title ?></title>
+    <style>
+        h3 {
+            margin: 0px;
+        }
+    </style>
+    <?php
     include("../navBar.php");
 ?>
 
 <div style="margin-left: 10px; padding-left: 10px;">
+<br>
 For details, click on the formations below.
 </div>
